e408643d7c92835fbb36fd956b155d0934c9f181
kieroid <kieran@lavato.net>
2025-04-27 18:55:10 -0500

    add websocket services (boost::beast)

--- MbsBackend.cpp
+++ MbsBackend.cpp
@@ -1,7 +1,30 @@
 #include "MbsBackend.h"
 #include "Util.cpp"
 
+//################################################
+// we will be using boost::beast for the webapi (including websockets)
+
+namespace beast = boost::beast;         // from <boost/beast.hpp>
+namespace http = beast::http;           // from <boost/beast/http.hpp>
+namespace net = boost::asio;            // from <boost/asio.hpp>
+using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
+
+
+//################################################
+
+
+
+
+
+
+
+
+//################################################
+
 int main(int argc, char* argv[]) {
-
-
+	if(argc < 2 || argc > 2) { // only one argument possible
+		std::cout << "Please include only one argument.";
+		return(1);
+	}
 }
+
---
+++ Util.cpp
@@ -0,0 +1,14 @@
+
+#include <boost/beast/core.hpp>
+#include <boost/beast/websocket.hpp>
+#include <boost/asio/connect.hpp>
+#include <boost/asio/ip/tcp.hpp>
+
+#include <cstdlib>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <thread>
+
+#include <sqlite3.h>
+
